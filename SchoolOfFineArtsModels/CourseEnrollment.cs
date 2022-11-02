756aa05a9912e8f1b7c1349dd4b794b625cb6099
Mark Rimbaugh <mark.rimbaugh@outlook.com>
2022-11-02 11:53:17 -0400

    added vwCourseInfo migration

--- SchoolOfFineArtsModels/CourseEnrollment.cs
+++ SchoolOfFineArtsModels/CourseEnrollment.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace SchoolOfFineArtsModels
 {
+    [Index(nameof(StudentId), nameof(CourseId), IsUnique = true)]
     public class CourseEnrollment
     {
         [Key]
