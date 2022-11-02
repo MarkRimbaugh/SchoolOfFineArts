d0bf04a335951d54463eb7914f34a4aa7f88f51a
Mark Rimbaugh <mark.rimbaugh@outlook.com>
2022-11-02 12:06:39 -0400

    added DbSet for view

---
+++ SchoolOfFineArtsModels/DTOs/CourseInfoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOfFineArtsModels.DTOs
+{
+    public class CourseInfoDTO
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public int TeacherId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string CourseName { get; set; } = string.Empty;
+        public string Abbreviation { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string TeacherName { get; set; } = string.Empty;
+
+
+    }
+}
